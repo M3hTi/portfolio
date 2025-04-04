d15c93edd80c95ca46d9f99eeb1f270cd6bdff7a
M3hTi <mehdinasir502@yahoo.com>
2025-04-04 22:51:13 +0330

    feat: 🎨 revamp About page with new layout, styles, and interactive elements

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,49 @@
+import styles from "./About.module.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { IoHome } from "react-icons/io5";
+import { Link } from "react-router-dom";
 function About() {
-    return (
-        <div>
-            this is About page!
+  return (
+    <main>
+      <div className={styles.sectionContainer}>
+        <div className={styles.headerContainer}>
+          <h1>About Me</h1>
+          <p>Front-End Developer | Gamer</p>
         </div>
-    )
+        <div className={styles.imgContainer}>
+          <img src="../../public/user.jpg" alt="" />
+        </div>
+        <div className={styles.aboutMain}>
+          <div className={styles.aboutMainLeft}>
+            <h3 className={styles.aboutSubHeading}>Developer</h3>
+            <p className={styles.aboutSubHeadingDetails}>
+              A junior front-end developer who loves coding and learning, always
+              tries to keep up with advancing technologies, and is a graduate of
+              Azad University, South Tehran branch.
+            </p>
+          </div>
+          <div className={styles.aboutMainRight}>
+            <DotLottieReact
+              src="https://lottie.host/b0464e3f-2d1f-461e-9d99-7ac556768935/K7PLL0mnzc.lottie"
+              loop
+              autoplay
+            />
+          </div>
+        </div>
+        <div className={styles.footerLink}>
+          Check out my <a href="https://github.com/M3hTi" target="_blank" className="clear-links">projects</a>
+        </div>
+        <div className={styles.vectorFrame}>
+          <img src="../../public/vector.png" alt="" />
+        </div>
+      </div>
+      <button className={`go-home-Btn ${styles.homeBg}`}>
+        <Link to="/">
+          <IoHome className="homeIcon" />
+        </Link>
+      </button>
+    </main>
+  );
 }
 
-export default About
+export default About;
