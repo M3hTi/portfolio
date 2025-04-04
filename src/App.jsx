77ef2f4a1aeb9d43d72619ec2268c12127bdecc9
M3hTi <mehdinasir502@yahoo.com>
2025-04-04 15:29:16 +0330

    feat: 🚀 implement main pages and update routing in App component to include Home, About, Contact, and NotFound pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={} />
-          <Route path="/about" element={} />
-          <Route path="/contact" element={} />
-          <Route path="*" element={} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div>
+            This is page which didn't find!
+        </div>
+    )
+}
+
+export default NotFound
