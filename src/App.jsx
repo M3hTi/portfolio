0dd77b022e92025028fc49477cee39cbfc63273f
M3hTi <mehdinasir502@yahoo.com>
2025-04-04 15:21:52 +0330

    feat: set up routing in App component using React Router, adding placeholder routes for home, about, contact, and a catch-all route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={} />
+          <Route path="/about" element={} />
+          <Route path="/contact" element={} />
+          <Route path="*" element={} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
