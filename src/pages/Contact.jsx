b3152dc05ef55d73bf09079e07faf163ef1a8960
M3hTi <mehdinasir502@yahoo.com>
2025-04-04 23:50:16 +0330

    feat: ✨ add email and message input fields with focus functionality to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+
 import { IoHome } from "react-icons/io5";
 import sharedStyles from "../components/shared.module.css";
 import styles from "./Contact.module.css";
 
 function Contact() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    console.log(myRef.current);
+    myRef.current.focus();
+  }, []);
   return (
     <main>
       <div className={sharedStyles.sectionContainer}>
@@ -19,12 +30,17 @@ function Contact() {
               id="email"
               placeholder="Enter your email"
               className={styles.inputBox}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              ref={myRef}
             />
             <textarea
               name="message"
               id="message"
               placeholder="Your Message"
               className={`${styles.inputBox} ${styles.bodyInput}`}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
             <button type="submit" className={styles.contactBtn}>
               Send Email
