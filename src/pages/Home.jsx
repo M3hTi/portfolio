25567056865956aebd9a51b5a4543dd4f69aae9d
M3hTi <mehdinasir502@yahoo.com>
2025-04-04 15:50:58 +0330

    feat: 🎨 update Home component with new header layout and styling enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 function Home() {
   return (
     <main>
-      <div className={styles.homeContainer}>This is Home page!</div>
+      <div className={styles.homeContainer}>
+        <div className={styles.headerText}>
+          <h1>Welcome to my portfolio!</h1>
+          <p>
+            This is Mehdi Khademi, a <u>Front-End Developer.</u>
+          </p>
+        </div>
+        <div className={styles.headerBtns}>
+          <Link to="">Know more about me</Link>
+          <Link to="">Contact with me</Link>
+        </div>
+        <div></div>
+      </div>
     </main>
   );
 }
