ebfca69a1c6e367c40fc0c8b5b05c63a392ceb3d
M3hTi <mehdinasir502@yahoo.com>
2025-04-05 18:26:20 +0330

    feat: ✨ add SideBar component with navigation links and toggle functionality

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import styles from "./SideBar.module.css";
+import { NavLink } from "react-router-dom";
+
+function SideBar() {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+
+  useEffect(() => {
+    function handleKey(e) {
+      e.preventDefault();
+      console.log(e.key);
+      if (e.key === "Escape") {
+        setIsSideBarOpen(false);
+      }
+    }
+    document.addEventListener("keydown", handleKey);
+
+    return () => document.removeEventListener("keydown", handleKey);
+  }, []);
+  return (
+    <>
+      <div className={styles.burgerContainer}>
+        <button
+          className={styles.menuBtn}
+          onClick={() => setIsSideBarOpen((s) => !s)}
+        >
+          <GiHamburgerMenu />
+        </button>
+      </div>
+      {isSideBarOpen && (
+        <>
+          <div className={`${styles.menuWrap}`}>
+            <div className={styles.menu}>
+              <nav className={styles.itemList}>
+                <ul>
+                  <li>
+                    <NavLink to="/" className="clear-nav-link">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/about" className="clear-nav-link">
+                      About
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact" className="clear-nav-link">
+                      Contact
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className={styles.crossBtn}>
+              <button onClick={() => setIsSideBarOpen(false)}>
+                <IoClose />
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default SideBar;
