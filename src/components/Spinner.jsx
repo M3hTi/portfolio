eadf4f362682252d6fada9f683d548196cd4a044
M3hTi <mehdinasir502@yahoo.com>
2025-05-16 19:21:42 +0330

    ✨ Feat
    Add Spinner component to display a loading indicator using Trio from ldrs/react.

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import "ldrs/react/Trio.css";
+
+import { Trio } from "ldrs/react";
+
+function Spinner() {
+  return (
+    // Default values shown
+    <Trio size="40" speed="1.3" color="black" />
+  );
+}
+
+export default Spinner;
