4c30450b6c84d7a005d1e95edd5376755f68960a
M3hTi <mehdinasir502@yahoo.com>
2025-05-17 12:22:08 +0330

    ♻️ Refactor
    Simply the Skills component by replacing individual skill divs with a dynamic rendering approach using a Skill component and an array of skill objects. This enhances maintainability and scalability. Additionally, removed unused CSS styles related to individual skill layout.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,34 +1,24 @@
 import { FaCss3, FaHtml5, FaReact } from "react-icons/fa";
 import { SiJavascript, SiReactquery, SiRedux } from "react-icons/si";
 
+import Skill from "./Skill";
 import styles from "./Skills.module.css";
+
+const mySkills = [
+  { icon: <FaHtml5 />, name: "HTML" },
+  { icon: <FaCss3 />, name: "CSS" },
+  { icon: <SiJavascript />, name: "JavaScript" },
+  { icon: <FaReact />, name: "React" },
+  { icon: <SiRedux />, name: "Redux" },
+  { icon: <SiReactquery />, name: "React Query" },
+];
+
 function Skills() {
   return (
     <div className={styles["skill-cards-container"]}>
-      <div className={styles.skill}>
-        <FaHtml5 style={{ fontSize: "120px", marginBottom: "15px" }} />
-        <p>HTML</p>
-      </div>
-      <div className={styles.skill}>
-        <FaCss3 style={{ fontSize: "120px", marginBottom: "15px" }} />
-        <p>CSS</p>
-      </div>
-      <div className={styles.skill}>
-        <SiJavascript style={{ fontSize: "120px", marginBottom: "15px" }} />
-        <p>JavaScript</p>
-      </div>
-      <div className={styles.skill}>
-        <FaReact style={{ fontSize: "120px", marginBottom: "15px" }} />
-        <p>React</p>
-      </div>
-      <div className={styles.skill}>
-        <SiRedux style={{ fontSize: "120px", marginBottom: "15px" }} />
-        <p>Redux</p>
-      </div>
-      <div className={styles.skill}>
-        <SiReactquery style={{ fontSize: "120px", marginBottom: "15px" }} />
-        <p>React Query</p>
-      </div>
+      {Array.from({ length: mySkills.length }, (_, i) => (
+        <Skill key={i} skillObj={mySkills.at(i)} />
+      ))}
     </div>
   );
 }
